59712f78a8afa00584ed783060c9ce1cede704d3
unknown <sainttrowagmail.com>
2025-03-12 17:47:52 +0800

    Print the result of the first number minus all of the remaining numbers

--- print_the_result_of_the_first_number_minus_all_of_the_remainding_numbers.py
+++ print_the_result_of_the_first_number_minus_all_of_the_remainding_numbers.py
@@ -5,3 +5,6 @@ for remaining_numbers in range(9):
     number_input = int(input(f"Input your number({remaining_numbers + 2}): "))
     number_list.append(number_input)
 #Print the result of the first number minus all of the remaining numbers
+sum_of_remaining_numbers = sum(number_list)
+result = number1 - sum_of_remaining_numbers
+print("First number minus all remaining numbers:", result)
