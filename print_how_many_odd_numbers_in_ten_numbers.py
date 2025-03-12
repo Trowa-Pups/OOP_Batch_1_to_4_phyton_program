912ddc03e5f05e43919c4ba414ded45670d4aedb
unknown <sainttrowagmail.com>
2025-03-12 12:40:43 +0800

    Ask the user to input ten numbers

--- print_how_many_odd_numbers_in_ten_numbers.py
+++ print_how_many_odd_numbers_in_ten_numbers.py
@@ -1,2 +1,7 @@
 #Ask the user to input ten numbers
+number_list = []
+for i in range(10): #To store the numbers inputed by the user
+    number_input = int(input(f"Input your number({i + 1}): "))
+    number_list.append(number_input) #To put the number inputed by the user to the list
+    
 #Print how many are odd numbers in the ten numbers
