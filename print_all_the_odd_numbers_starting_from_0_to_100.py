931d304f54c91b2e9d6e304828cdea9c00a9e8fa
unknown <sainttrowagmail.com>
2025-03-12 20:06:55 +0800

    Fixed line 3

--- print_all_the_odd_numbers_starting_from_0_to_100.py
+++ print_all_the_odd_numbers_starting_from_0_to_100.py
@@ -1,4 +1,4 @@
 #Use "for" loop and check if the number is odd
 for number in range(101): #Use (101) because (100) is only 0 to 99
-    if number != 0: #To check if the number is odd
-#Print all the odd numbers
+    if number % 2 != 0: #To check if the number is odd
+        print(number) #Print all the odd numbers
