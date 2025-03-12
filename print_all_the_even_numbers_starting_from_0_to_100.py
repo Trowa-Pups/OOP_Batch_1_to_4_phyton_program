83a046ff3a657e9d73319c055b756f6572dfbab1
unknown <sainttrowagmail.com>
2025-03-12 20:02:57 +0800

    Comment added

--- print_all_the_even_numbers_starting_from_0_to_100.py
+++ print_all_the_even_numbers_starting_from_0_to_100.py
@@ -1,5 +1,5 @@
 #Use "for" loop and check if the number is even
 print("Even numbers:")
 for number in range(101): #Use (101) because (100) is only 0 to 99
-    if number % 2 ==0:
+    if number % 2 ==0: #To check if number is even
         print(number) #Print all the even numbers
