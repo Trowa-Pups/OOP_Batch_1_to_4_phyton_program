3c51eba5626b97d5cf185ca8ec9ce650db97db31
unknown <sainttrowagmail.com>
2025-03-12 10:32:27 +0800

    Ask the user to input two numbers

--- print_Equal_when_the_numbers_are_the_same.py
+++ print_Equal_when_the_numbers_are_the_same.py
@@ -1,2 +1,5 @@
 #Ask the user to input two numbers
+number1 = int(input("Input your first number: "))
+number2 = int(input("Input your second number: "))
+
 #Print "Equal" when the numbers are the same
