ae3635553afeb711dac73f00ad99dc3df9006ba7
unknown <sainttrowagmail.com>
2025-03-12 10:26:41 +0800

    Print the bigger number

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -1,5 +1,10 @@
 #Ask the user to input two numbers
-
 number1 = int(input("Input your first number: "))
 number2 = int(input("Input your second number: "))
+
 #Print the bigger number
+if number1 > number2:
+    print("The bigger number is Number 1: ", number1)
+
+else:
+    print("The bigger number is Number 2: ",number2)
