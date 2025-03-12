0ee175bca40b87402098ffa6c6117e11af4c8109
unknown <sainttrowagmail.com>
2025-03-12 19:35:47 +0800

    Print all the even numbers from ten numbers and added comments

--- print_all_the_even_numbers_from_ten_numbers.py
+++ print_all_the_even_numbers_from_ten_numbers.py
@@ -1,7 +1,10 @@
 #Ask the user to input ten numbers
-number_list = []
+number_list = [] #To store numbers inputed by the user
 for i in range(10):
     number_input = int(input(f"Input your number({i + 1}): "))
-    number_list.append(number_input)
+    number_list.append(number_input) #To put the number inputed by the user to the list
 
 #Print all the even numbers from ten numbers
+for number in number_list: #To grab a number in the list
+    if number % == 0: #To check if it is even
+        print(number)
