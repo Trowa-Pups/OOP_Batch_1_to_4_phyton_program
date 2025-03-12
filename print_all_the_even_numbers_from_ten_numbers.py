17752adedfdaa11c68693d15408d194fed30eb40
unknown <sainttrowagmail.com>
2025-03-12 19:25:34 +0800

    Ask the user to input ten numbers

--- print_all_the_even_numbers_from_ten_numbers.py
+++ print_all_the_even_numbers_from_ten_numbers.py
@@ -1,2 +1,7 @@
 #Ask the user to input ten numbers
+number_list = []
+for i in range(10):
+    number_input = int(input(f"Input your number({i + 1}): "))
+    number_list.append(number_input)
+
 #Print all the even numbers from ten numbers
