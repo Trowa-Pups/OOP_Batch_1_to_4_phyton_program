6fbec21e7c0857cc777b00989f7fc066320cf177
unknown <sainttrowagmail.com>
2025-03-12 10:34:54 +0800

    Print 'Equal' when the numbers are the same

--- print_Equal_when_the_numbers_are_the_same.py
+++ print_Equal_when_the_numbers_are_the_same.py
@@ -3,3 +3,8 @@ number1 = int(input("Input your first number: "))
 number2 = int(input("Input your second number: "))
 
 #Print "Equal" when the numbers are the same
+if number1 == number2:
+    print("Equal")
+
+else:
+    print("Not Equal")
