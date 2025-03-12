40a74697f5d6556f8d5485769174ee621884c7de
unknown <sainttrowagmail.com>
2025-03-12 13:32:20 +0800

    Use 'for' loop and check if the number ends with a zero

--- print_all_the_numbers_starting_from_0_to_100_expect_numbers_ending_in_zero.py
+++ print_all_the_numbers_starting_from_0_to_100_expect_numbers_ending_in_zero.py
@@ -1,2 +1,4 @@
 #Use "for" loop and check if the number ends with a zero
+for number in range(101): #Use (101) because (100) is only 0 to 99
+    if number % 10 != 0: #If the remainder of the number is 0 when % by 10, it ends with a zero
 #Print the numbers without ending in a zero
