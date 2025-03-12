94e8dfd9b92fe379ce1dce2ad71f479749697dbf
unknown <sainttrowagmail.com>
2025-03-12 10:22:16 +0800

    Ask the user to input two numbers

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -1,2 +1,5 @@
-#Ask to user to input two numbers
+#Ask the user to input two numbers
+
+number1 = int(input("Input your first number: "))
+number2 = int(input("Input your second number: "))
 #Print the bigger number
