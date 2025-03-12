fb2c495d396e25a4badd28fd32d9a9a6db8c252a
unknown <sainttrowagmail.com>
2025-03-12 16:58:55 +0800

    Print the difference of the two numbers

--- print_the_difference_of_the_two_numbers.py
+++ print_the_difference_of_the_two_numbers.py
@@ -3,3 +3,5 @@ number1 = int(input("Please input your first number: "))
 number2 = int(input("Please input your second number: "))
 
 #Print the difference of the two numbers
+difference = number1 - number2 
+print("Difference of the two numbers: ", difference)
