9db2bd8b03b334e9f0bf4bbea49c907bc446bf12
unknown <sainttrowagmail.com>
2025-03-12 12:18:36 +0800

    Ask the user to input ten numbers

--- print_the_sum_of_all_ten_numbers.py
+++ print_the_sum_of_all_ten_numbers.py
@@ -1,2 +1,7 @@
 #Ask the user to input ten numbers
+numbers = [] #To store the numbers inputed by the user
+for i in range(10):
+    number_input = int(input(f"Input your number ({i + 1}): "))
+    numbers.append(number_input)
+
 #Print the sum of all the numbers
