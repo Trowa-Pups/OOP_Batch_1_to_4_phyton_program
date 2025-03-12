1908f398d581cbabea5ba058104568f929b5926d
unknown <sainttrowagmail.com>
2025-03-12 19:32:22 +0800

    COmment fix

--- print_the_result_of_the_first_number_minus_all_of_the_remainding_numbers.py
+++ print_the_result_of_the_first_number_minus_all_of_the_remainding_numbers.py
@@ -1,9 +1,9 @@
 #Ask the user to input ten numbers
 number1 = int(input("Input your number(1): "))
-number_list = []
+number_list = [] #To store the numbers inputed by the user
 for remaining_numbers in range(9):
     number_input = int(input(f"Input your number({remaining_numbers + 2}): "))
-    number_list.append(number_input)
+    number_list.append(number_input) 
 #Print the result of the first number minus all of the remaining numbers
 sum_of_remaining_numbers = sum(number_list)
 result = number1 - sum_of_remaining_numbers
