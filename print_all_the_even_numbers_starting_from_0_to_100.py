855913eb6453759546080363ce0d2a13f6f4de69
unknown <sainttrowagmail.com>
2025-03-12 13:26:01 +0800

    Fix the spelling of the comment

--- print_all_the_even_numbers_starting_from_0_to_100.py
+++ print_all_the_even_numbers_starting_from_0_to_100.py
@@ -2,4 +2,4 @@
 print("Even numbers:")
 for number in range(101): #Use (101) because (100) is only 0 to 99
     if number % 2 ==0:
-        print(number) #Print all the even number
+        print(number) #Print all the even numbers
