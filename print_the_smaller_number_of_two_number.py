b746f6b44b181ea63cdba862c402599ab3614475
unknown <sainttrowagmail.com>
2025-03-12 16:21:58 +0800

    Print the smaller number

--- print_the_smaller_number_of_two_number.py
+++ print_the_smaller_number_of_two_number.py
@@ -3,3 +3,8 @@ number1 = int(input("Please input your first number: "))
 number2 = int(input("Please input your second number: "))
 
 #Print the smaller number
+if number1 > number2:
+    print("Smaller Number: ", number2)
+
+else:
+    print("Smaller Number: ", number1)
