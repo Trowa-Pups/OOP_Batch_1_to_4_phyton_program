ca86b09e36a10012cb57a44067e5914be383321d
unknown <sainttrowagmail.com>
2025-03-12 17:36:02 +0800

    Ask the user to input two numbers

--- print_the_remainder_when_the_first_number_is_divided_by_second_number.py
+++ print_the_remainder_when_the_first_number_is_divided_by_second_number.py
@@ -1,2 +1,5 @@
 #Ask the user to input two numbers
+number1 = int(input("Please input your first number: "))
+number2 = int(input("Please input your second number: "))
+
 #Print the remainder when the first number is divided by thge second number
