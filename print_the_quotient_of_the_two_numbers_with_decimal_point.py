f16ad0885eda26a1d7d6e245fad23fbfbcad9a4e
unknown <sainttrowagmail.com>
2025-03-12 11:07:48 +0800

    Print the quotient of the two numbers with the decimal point

--- print_the_quotient_of_the_two_numbers_with_decimal_point.py
+++ print_the_quotient_of_the_two_numbers_with_decimal_point.py
@@ -2,4 +2,6 @@
 number1 = int(input("Print your first number: "))
 number2 = int(input("Print your second number: "))
 
-#Print the quotient of the two numbers with the decimal point"
+#Print the quotient of the two numbers with the decimal point
+quotient_numbers = number1 / number2
+print(quotient_numbers)
