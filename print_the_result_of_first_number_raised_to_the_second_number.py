e65d48ee97621aa6ca7df07fde6db7129ea68edc
unknown <sainttrowagmail.com>
2025-03-12 11:26:35 +0800

    Print the result when the first number is raised to the second number

--- print_the_result_of_first_number_raised_to_the_second_number.py
+++ print_the_result_of_first_number_raised_to_the_second_number.py
@@ -3,3 +3,5 @@ number1 = int(input("Input your first number: "))
 number2 = int(input("Input your second number: "))
 
 #Print the result when the first number is raised to the second number
+raised_result = number1 ** number2
+print(raised_result)
