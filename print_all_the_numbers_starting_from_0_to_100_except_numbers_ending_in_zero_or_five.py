0a497c6d4da376e7a82c40a5e92b003db6d849ef
unknown <sainttrowagmail.com>
2025-03-12 20:15:43 +0800

    comment fix

--- print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_or_five.py
+++ print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_or_five.py
@@ -1,2 +1,2 @@
 #Use "for" loop and check if the number ends in a zero or five
-#Print all the numbers starting from 0 to 100 except numbers ending in zero or five
+#Print the numbers without ending in zero or five 
