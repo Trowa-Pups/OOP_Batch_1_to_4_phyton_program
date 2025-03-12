4054757add3f5724f68a0f5bfb48e79b2587760e
unknown <sainttrowagmail.com>
2025-03-12 12:08:18 +0800

    Print the bigger number

--- print_the_bigger_number_of_two_numbers.py
+++ print_the_bigger_number_of_two_numbers.py
@@ -3,3 +3,8 @@ number1 = int(input("Please input your first number: "))
 number2 = int(input("Please input your second number: "))
 
 #Print the bigger number
+if number1 > number2:
+    print("Bigger number:", number1)
+
+else:
+    print("Bigger number:", number2)
