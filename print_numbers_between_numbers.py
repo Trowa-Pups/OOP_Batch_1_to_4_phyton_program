ccb4e37360572feb0a0329569b8df4f6a79232e6
unknown <sainttrowagmail.com>
2025-03-12 20:36:05 +0800

    Ask the user to input two numbers

--- print_numbers_between_numbers.py
+++ print_numbers_between_numbers.py
@@ -1,2 +1,4 @@
 #Ask the user to input two numbers
+number1 = int(input("Please input your first number: "))
+number2 = int(input("Please input your second number: "))
 #Print all the numbers between the two numbers
