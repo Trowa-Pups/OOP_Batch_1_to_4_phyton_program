cac96001b2cda4fb371b6dd4d2ffa1b5454c5dc1
unknown <sainttrowagmail.com>
2025-03-12 11:04:55 +0800

    ask the user to input two numbers

--- print_the_quotient_of_the_two_numbers_with_decimal_point.py
+++ print_the_quotient_of_the_two_numbers_with_decimal_point.py
@@ -1,2 +1,5 @@
 #Ask the user to input two numbers
+number1 = int(input("Print your first number: "))
+number2 = int(input("Print your second number: "))
+
 #Print the quotient of the two numbers with the decimal point"
