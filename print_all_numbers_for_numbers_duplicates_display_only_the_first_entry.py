f54abafc14cfe2c49f03d2014101e92f49c9c8dc
unknown <sainttrowagmail.com>
2025-03-13 13:41:27 +0800

    pseudocode

---
+++ print_all_numbers_for_numbers_duplicates_display_only_the_first_entry.py
@@ -0,0 +1,3 @@
+#Ask the user to input ten numbers
+#Create a list and have a system check if it has duplites, and only display/print the first entry
+#Display/Print all the numbers and first entries
