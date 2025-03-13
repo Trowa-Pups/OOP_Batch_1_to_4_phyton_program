412b39ec6daace55eac0f939392245d21295909d
unknown <sainttrowagmail.com>
2025-03-13 19:41:44 +0800

    Ask the user to input numbers until they inputed an invalid input

--- display_average_of_all_numbers_until_invalid_input.py
+++ display_average_of_all_numbers_until_invalid_input.py
@@ -1,3 +1,19 @@
 #Ask the user to input numbers until they inputed an invalid input
+number_list = [] #To store the numbers inputed
+current_number = 1
+
+while True:
+    try:
+        print("Input something other than a number to stop")
+    
+        number = int(input(f"Input number ({current_number}): "))
+
+        current_number += 1
+
+        number_list.append(number)
+        
+    except ValueError:
+        print("Stopped")
+        break
 #Use mean.() to get the average
 #Print the average of all numbers
