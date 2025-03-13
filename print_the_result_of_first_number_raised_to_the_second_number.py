d0bc5b5e2572a8a5d1960c957f21c1b1275c1234
unknown <sainttrowagmail.com>
2025-03-13 17:43:17 +0800

    spacing fix

--- print_the_result_of_first_number_raised_to_the_second_number.py
+++ print_the_result_of_first_number_raised_to_the_second_number.py
@@ -4,4 +4,5 @@ number2 = int(input("Input your second number: "))
 
 #Print the result when the first number is raised to the second number
 raised_result = number1 ** number2
+
 print(raised_result)
