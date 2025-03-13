df7ba2bc63ede42d91d6b0620515d21a7e9d6377
unknown <sainttrowagmail.com>
2025-03-13 17:28:46 +0800

    spacing fix

--- print_all_numbers_for_numbers_duplicates_display_only_the_first_entry.py
+++ print_all_numbers_for_numbers_duplicates_display_only_the_first_entry.py
@@ -1,10 +1,12 @@
 #Ask the user to input ten numbers
 number_list = [] #Create a list and have a system check if it has duplicates, and only display/print the first entry
 duplicate_list = [] #To store the duplicates
+
 for i in range(10):
     number = int(input(f"Input your number({i + 1}): "))
     if number in number_list: #To check if its a duplicate or not
         duplicate_list.append(number) #If it is a duplicate, it adds it in the duplicate list
+
     else:
         number_list.append(number) #If its not, it adds it in the number list
 
