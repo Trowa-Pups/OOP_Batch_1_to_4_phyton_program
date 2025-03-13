05705e9a4d64c27be29ba37d36f376aa2c524a01
unknown <sainttrowagmail.com>
2025-03-13 18:27:43 +0800

    spacing fix

--- print_all_the_numbers_with_a_duplicate_from_ten_numbers.py
+++ print_all_the_numbers_with_a_duplicate_from_ten_numbers.py
@@ -6,5 +6,4 @@ for i in range(10):
 
 for check in number_list:
     if number_list.count(check) != 1: #To check if it is a duplicate
-        
         print(check) #Print the numbers with duplicates
