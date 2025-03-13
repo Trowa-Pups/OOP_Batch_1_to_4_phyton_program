5b866e23731b49750b030ced7b107db10e55fe68
unknown <sainttrowagmail.com>
2025-03-13 18:57:43 +0800

    ask the user to input numbers until they inputed an invalid input

--- until_user_input_is_invalid_display_highest_number.py
+++ until_user_input_is_invalid_display_highest_number.py
@@ -1,3 +1,15 @@
 #Ask the user to input numbers until they inputed an invalid input
+current_number = 1
+while True:
+    try:
+        print("Input something other than a number to stop")
+    
+        number = int(input(f"Input number ({current_number}): "))
+
+        current_number += 1
+    except ValueError:
+        print("Stopped")
+        break
+
 #Create a list and check what number in the list has the highest value
 #Print the highest number
