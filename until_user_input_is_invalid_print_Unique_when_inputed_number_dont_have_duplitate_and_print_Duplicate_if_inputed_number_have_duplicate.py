c866f3fb76dcbe08038724ae69a0b66353f261a6
unknown <sainttrowagmail.com>
2025-03-13 15:20:49 +0800

    pseudocode

---
+++ until_user_input_is_invalid_print_Unique_when_inputed_number_dont_have_duplitate_and_print_Duplicate_if_inputed_number_have_duplicate.py
@@ -0,0 +1,3 @@
+#Ask the user to input until they inputed an invalid input
+#Check if the number has duplicated or not
+#Print Unique if number has no duplicate, and print Duplicate if number has duplicate
