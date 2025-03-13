b2218caf733d47aa80b5423f34afb329f8e41ef4
unknown <sainttrowagmail.com>
2025-03-13 17:19:56 +0800

    Create a list that has a system where it grabs a number in the list and check if its a duplicate of another number

--- print_all_the_numbers_with_a_duplicate_from_ten_numbers.py
+++ print_all_the_numbers_with_a_duplicate_from_ten_numbers.py
@@ -1,6 +1,9 @@
 #Ask the user to input ten numbers
+number_list = [] #Create a list that has a system where it grabs a number in the list and check if its a duplicate of another number
 for i in range(10):
     number = int(input(f"Input your number({i + 1}): "))
-    
-#Create a list that has a system where it grabs a number in the list and check if its a duplicate of another number
+    number_list.append(number)
+
+for check in number_list:
+    if number_list.count(check) != 1: #To check if it is a duplicate
 #Print the numbers with duplicates
