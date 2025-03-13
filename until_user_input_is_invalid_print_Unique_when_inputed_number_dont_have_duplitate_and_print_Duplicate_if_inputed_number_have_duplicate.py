90f1b819f3deed23b33b65484fdf045e3bf45e6b
unknown <sainttrowagmail.com>
2025-03-13 15:29:59 +0800

    Ask the user to input until they inputed an invalid inputed

--- until_user_input_is_invalid_print_Unique_when_inputed_number_dont_have_duplitate_and_print_Duplicate_if_inputed_number_have_duplicate.py
+++ until_user_input_is_invalid_print_Unique_when_inputed_number_dont_have_duplitate_and_print_Duplicate_if_inputed_number_have_duplicate.py
@@ -1,3 +1,13 @@
 #Ask the user to input until they inputed an invalid input
+current_number = 1 
+while True:
+    try:
+        print("Input something other than a number to stop")
+        number = int(input(f"Input number ({current_number}): "))
+        current_number += 1
+    
+    except ValueError:
+        print("Stopped")
+        break
 #Check if the number has duplicated or not
 #Print Unique if number has no duplicate, and print Duplicate if number has duplicate
