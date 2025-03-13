99b6b2d71487e978bb0d1103257d7076ffc9119d
unknown <sainttrowagmail.com>
2025-03-13 19:01:13 +0800

    Print the highest number

--- until_user_input_is_invalid_display_highest_number.py
+++ until_user_input_is_invalid_display_highest_number.py
@@ -1,4 +1,5 @@
 #Ask the user to input numbers until they inputed an invalid input
+number_list = [] #Create a list and check what number in the list has the highest value
 current_number = 1
 while True:
     try:
@@ -7,9 +8,12 @@ while True:
         number = int(input(f"Input number ({current_number}): "))
 
         current_number += 1
+
+        number_list.append(number)
+        
     except ValueError:
         print("Stopped")
         break
 
-#Create a list and check what number in the list has the highest value
-#Print the highest number
+if number_list: # Make sure the list isn't empty
+    print("Highest Number:", max(number_list))#Print the highest number
