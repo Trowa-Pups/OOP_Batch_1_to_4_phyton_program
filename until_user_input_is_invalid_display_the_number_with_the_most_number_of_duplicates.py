bd6812208320ad94c658dd060db6ced34651af74
unknown <sainttrowagmail.com>
2025-03-13 18:00:54 +0800

    pseudocode

---
+++ until_user_input_is_invalid_display_the_number_with_the_most_number_of_duplicates.py
@@ -0,0 +1,4 @@
+#Ask the user to input numbers until they inputed an invalid input
+#Create a list that has a system where it checks if inputed number has a duplicate or not 
+#Check how many duplicates a number has
+#Print the number with the most number of duplicates
