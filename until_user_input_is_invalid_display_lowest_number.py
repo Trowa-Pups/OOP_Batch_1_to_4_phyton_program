29fa8b9e62f692797f7795bf674104281072ff42
unknown <sainttrowagmail.com>
2025-03-13 16:20:56 +0800

    Ask the user to input numbers until they inputed an invalid input

--- until_user_input_is_invalid_display_lowest_number.py
+++ until_user_input_is_invalid_display_lowest_number.py
@@ -1,3 +1,14 @@
 #Ask the user to input numbers until they inputed an invalid input
+current_number = 1
+
+while True:
+    try:
+        print("Input something other than a number to stop")
+        number = int(input(f"Input number ({current_number}): "))
+        current_number += 1
+
+    except ValueError:
+        print("Stopped")
+        break
 #Create a list and check what number in the list has the lowest value
 #Print the lowest number
