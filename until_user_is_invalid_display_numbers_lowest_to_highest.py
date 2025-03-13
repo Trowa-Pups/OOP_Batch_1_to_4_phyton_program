a36dfe4a7e60df61d8302840fbfc18a5dc1ea6ee
unknown <sainttrowagmail.com>
2025-03-13 17:54:22 +0800

    spacing issue

--- until_user_is_invalid_display_numbers_lowest_to_highest.py
+++ until_user_is_invalid_display_numbers_lowest_to_highest.py
@@ -4,10 +4,13 @@ number_list = [] #To store inputed numbers by the user
 while True:
     try:
         print("Input something other than a number to stop")
+        
         number = int(input(f"Input number({current_number}): "))
-        number_list.append(number)
-        current_number += 1
 
+        number_list.append(number)
+
+        current_number += 1
+        
     except ValueError:
         print("Stopped")
         break
