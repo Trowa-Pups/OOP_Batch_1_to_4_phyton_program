f2bf26ff23b8edc99f8cd73b005db20a89cc599f
unknown <sainttrowagmail.com>
2025-03-13 17:36:43 +0800

    spacing fix

--- print_Not_Equal_when_the_numbers_are_not_equal.py
+++ print_Not_Equal_when_the_numbers_are_not_equal.py
@@ -1,6 +1,7 @@
 #Ask the user for two numbers
 number1 = int(input("Please input your first number: "))
 number2 = int(input("Please input your second number: "))
+
 #Print "Not Equal" when the numbers are not the same
 if number1 != number2:
     print("Not Equal")
