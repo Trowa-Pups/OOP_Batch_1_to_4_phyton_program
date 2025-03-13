ec429758c6716e7f90e7c11cc98e4ff1f7b2f618
unknown <sainttrowagmail.com>
2025-03-13 17:14:30 +0800

    pseudocode

---
+++ print_all_the_numbers_with_a_duplicate_from_ten_numbers.py
@@ -0,0 +1,3 @@
+#Ask the user to input ten numbers
+#Create a list that has a system where it grabs a number in the list and check if its a duplicate of another number
+#Print the numbers with duplicates
