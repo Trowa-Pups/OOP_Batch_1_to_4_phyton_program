39fff66bc3d55abde3b18a1fa35b1d3ee18bee96
unknown <sainttrowagmail.com>
2025-03-13 17:37:22 +0800

    spacing fix

--- print_numbers_between_numbers.py
+++ print_numbers_between_numbers.py
@@ -1,6 +1,7 @@
 #Ask the user to input two numbers
 number1 = int(input("Please input your first number: "))
 number2 = int(input("Please input your second number: "))
+
 #Print all the numbers between the two numbers
 for number in range(number1 + 1, number2): #number1 + 1, because it also prints number1
     print(number)
