5283860e7089c1a7459e7ec91a8dcdf62a279da6
unknown <sainttrowagmail.com>
2025-03-13 17:16:34 +0800

    Ask the user to input ten numbers

--- print_all_the_numbers_with_a_duplicate_from_ten_numbers.py
+++ print_all_the_numbers_with_a_duplicate_from_ten_numbers.py
@@ -1,3 +1,6 @@
 #Ask the user to input ten numbers
+for i in range(10):
+    number = int(input(f"Input your number({i + 1}): "))
+    
 #Create a list that has a system where it grabs a number in the list and check if its a duplicate of another number
 #Print the numbers with duplicates
