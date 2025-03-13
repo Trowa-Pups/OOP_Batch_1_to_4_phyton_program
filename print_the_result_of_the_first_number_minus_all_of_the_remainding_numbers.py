66c3bc116cee5cbddc6e81b0980b75df28bacdfa
unknown <sainttrowagmail.com>
2025-03-13 17:30:10 +0800

    spacing fix

--- print_the_result_of_the_first_number_minus_all_of_the_remainding_numbers.py
+++ print_the_result_of_the_first_number_minus_all_of_the_remainding_numbers.py
@@ -1,10 +1,14 @@
 #Ask the user to input ten numbers
 number1 = int(input("Input your number(1): "))
 number_list = [] #To store the numbers inputed by the user
+
 for remaining_numbers in range(9):
     number_input = int(input(f"Input your number({remaining_numbers + 2}): "))
     number_list.append(number_input) 
+
 #Print the result of the first number minus all of the remaining numbers
 sum_of_remaining_numbers = sum(number_list)
+
 result = number1 - sum_of_remaining_numbers
+
 print("First number minus all remaining numbers:", result)
