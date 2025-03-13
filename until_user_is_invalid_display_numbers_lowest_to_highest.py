b3eb4f67905c2484e7adb1b0c764ec1ae2e50c40
unknown <sainttrowagmail.com>
2025-03-13 16:57:45 +0800

    Use sort() to sort numbers lowest to highest

--- until_user_is_invalid_display_numbers_lowest_to_highest.py
+++ until_user_is_invalid_display_numbers_lowest_to_highest.py
@@ -1,14 +1,16 @@
 #Ask the user to input numbers until they inputed an invalid input
 current_number = 1
+number_list = [] #To store inputed numbers by the user
 while True:
     try:
         print("Input something other than a number to stop")
         number = int(input(f"Input number({current_number}): "))
+        number_list.append(number)
         current_number += 1
 
     except ValueError:
         print("Stopped")
         break
 
-#Use sort() to sort numbers lowest to highest
-#Print the numbers lowest to highest
+if number_list: #To make sure list isn't empty
+    number_list.sort() #Use sort() to sort numbers lowest to highest
