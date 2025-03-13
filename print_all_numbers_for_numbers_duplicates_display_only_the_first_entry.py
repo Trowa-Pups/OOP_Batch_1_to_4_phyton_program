687518044f76b6a4ab24987f823ca712a8958775
unknown <sainttrowagmail.com>
2025-03-13 13:59:31 +0800

    Create a list and have a system check if it has duplicates, and only display/print the first entry

--- print_all_numbers_for_numbers_duplicates_display_only_the_first_entry.py
+++ print_all_numbers_for_numbers_duplicates_display_only_the_first_entry.py
@@ -1,5 +1,11 @@
 #Ask the user to input ten numbers
+number_list = [] #Create a list and have a system check if it has duplicates, and only display/print the first entry
+duplicate_list = [] #To store the duplicates
 for i in range(10):
     number = int(input(f"Input your number({i + 1}): "))
-#Create a list and have a system check if it has duplites, and only display/print the first entry
+    if number in number_list: #To check if its a duplicate or not
+        duplicate_list.append(number) #If it is a duplicate, it adds it in the duplicate list
+    else:
+        number_list.append(number) #If its not, it adds it in the number list
+
 #Display/Print all the numbers and first entries
