84e8f0b4742900427bbbc19974bccf3baa0f4fc6
unknown <sainttrowagmail.com>
2025-03-13 16:34:48 +0800

    Create a list, checks the lowest number, and Print the lowest numbers

--- until_user_input_is_invalid_display_lowest_number.py
+++ until_user_input_is_invalid_display_lowest_number.py
@@ -1,14 +1,16 @@
 #Ask the user to input numbers until they inputed an invalid input
 current_number = 1
-
+number_list = [] #Create a list and check what number in the list has the lowest value
 while True:
     try:
         print("Input something other than a number to stop")
         number = int(input(f"Input number ({current_number}): "))
         current_number += 1
+        number_list.append(number)
 
     except ValueError:
         print("Stopped")
         break
-#Create a list and check what number in the list has the lowest value
-#Print the lowest number
+
+if number_list: #Make sure the list isn't empty
+    print("Lowest Number:" , min(number_list)) #Print the lowest number
