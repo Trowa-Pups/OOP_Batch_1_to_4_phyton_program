68e25b4301242c93ea544fb13bfc81cfb20b3120
unknown <sainttrowagmail.com>
2025-03-13 18:09:35 +0800

    Ask the user to input numbers until they inputed an invalid input

--- until_user_input_is_invalid_display_the_number_with_the_most_number_of_duplicates.py
+++ until_user_input_is_invalid_display_the_number_with_the_most_number_of_duplicates.py
@@ -1,4 +1,15 @@
 #Ask the user to input numbers until they inputed an invalid input
+current_number = 1
+while True:
+    try: 
+        print("Input somnething other than a number to stop")
+
+        number = int(input(f"Input number ({current_number})"))
+
+    except ValueError:
+        print("Stopped")
+        break
+    
 #Create a list that has a system where it checks if inputed number has a duplicate or not 
 #Check how many duplicates a number has
 #Print the number with the most number of duplicates
