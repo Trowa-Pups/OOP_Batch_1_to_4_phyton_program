9ffc88f1e05c951e0ba60e68404a232ca3bd1b94
unknown <sainttrowagmail.com>
2025-03-13 16:09:26 +0800

    Changed the checking, and added comments to explain why, and 'Print UNique if number has no duplicated, and Duplicated if number has duplicate

--- until_user_input_is_invalid_print_Unique_when_inputed_number_dont_have_duplitate_and_print_Duplicate_if_inputed_number_have_duplicate.py
+++ until_user_input_is_invalid_print_Unique_when_inputed_number_dont_have_duplitate_and_print_Duplicate_if_inputed_number_have_duplicate.py
@@ -1,19 +1,24 @@
 #Ask the user to input until they inputed an invalid input
-number_list = []
-current_number = 1 
-while True:
+number_list = [] #To store inputed numbers by the user
+current_number = 1 #To see the current number the user inputed
+while True: 
     try:
         print("Input something other than a number to stop")
         number = int(input(f"Input number ({current_number}): "))
-        current_number += 1
-        number_list.append(number)
-
         #Check if the number has duplicated or not
-        for check in number_list: 
-            if number_list.count(check) == 1:
-    
+        #Print Unique if number has no duplicate, and print Duplicate if number has duplicate
+        if number in number_list: #Checks if the number if the number is already in the list
+            print("Duplicate")    #Changed it to that because its less complicated and does the same job
+
+        else:
+            print("Unique")
+
+        current_number += 1  
+        number_list.append(number) #To put it in the list
+        #I moved this because when i input a number, it puts it in the list first before checking
+        #So, everytime i input a number, the system already sees that the number is already there , so it prints "Duplicate" everytime
+
     except ValueError:
         print("Stopped")
         break
 
-#Print Unique if number has no duplicate, and print Duplicate if number has duplicate
