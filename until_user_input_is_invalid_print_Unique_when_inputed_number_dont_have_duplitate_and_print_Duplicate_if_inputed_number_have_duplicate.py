6b1332f6a7506edb883957208e7db04ddfaa11b0
unknown <sainttrowagmail.com>
2025-03-13 15:52:25 +0800

    Check if the number is duplicated or not

--- until_user_input_is_invalid_print_Unique_when_inputed_number_dont_have_duplitate_and_print_Duplicate_if_inputed_number_have_duplicate.py
+++ until_user_input_is_invalid_print_Unique_when_inputed_number_dont_have_duplitate_and_print_Duplicate_if_inputed_number_have_duplicate.py
@@ -1,13 +1,19 @@
 #Ask the user to input until they inputed an invalid input
+number_list = []
 current_number = 1 
 while True:
     try:
         print("Input something other than a number to stop")
         number = int(input(f"Input number ({current_number}): "))
         current_number += 1
+        number_list.append(number)
+
+        #Check if the number has duplicated or not
+        for check in number_list: 
+            if number_list.count(check) == 1:
     
     except ValueError:
         print("Stopped")
         break
-#Check if the number has duplicated or not
+
 #Print Unique if number has no duplicate, and print Duplicate if number has duplicate
