a143dde5b5540cdda09ce40cad35adcbee6df998
unknown <sainttrowagmail.com>
2025-03-13 13:44:21 +0800

    Ask the user to input ten numbers

--- print_all_numbers_for_numbers_duplicates_display_only_the_first_entry.py
+++ print_all_numbers_for_numbers_duplicates_display_only_the_first_entry.py
@@ -1,3 +1,5 @@
 #Ask the user to input ten numbers
+for i in range(10):
+    number = int(input(f"Input your number({i + 1}): "))
 #Create a list and have a system check if it has duplites, and only display/print the first entry
 #Display/Print all the numbers and first entries
