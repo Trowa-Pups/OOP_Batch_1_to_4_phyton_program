8f463ddec1e764c683fbb2343a4518471da84d7a
unknown <sainttrowagmail.com>
2025-03-13 19:17:19 +0800

    ask the user to input numbers until they inputed an invalid input

--- until_user_input_is_invalid_display_numbers_highest_to_lowest.py
+++ until_user_input_is_invalid_display_numbers_highest_to_lowest.py
@@ -1,3 +1,17 @@
 #Ask the user to input numbers until they inputed an invalid input
+current_number = 1
+while True:
+    try:
+
+        print("Input something other than a number to stop")
+
+        number = int(input(f"Input number ({current_number}): "))
+    
+        current_number += 1
+    
+    except ValueError:
+        print("Stopped")
+        break
+    
 #Use sort.() to sort numbers highest to lowest
 #Print the numbers highest to lowest
